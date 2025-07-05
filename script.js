82101f424e3cdbf1560129ac068c96a9f31de0ca
nikop0 <nikosp005@gmail.com>
2025-07-05 23:07:25 +0200

    main game updated/

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let playBtn = document.querySelector('#play-btn');
 let chooseItem = document.querySelector('.choose-item');
-
+let moves = document.querySelectorAll('.single-item');
+let chosenMove = null;
 playBtn.addEventListener('click', e => {
     playBtn.style.display = 'none';
     chooseItem.style.display = 'block';
 });
+
+moves.forEach( move => {
+    move.addEventListener('click', e => {
+    chosenMove = e.target;
+    chooseItem.style.display = 'none';
+})});
