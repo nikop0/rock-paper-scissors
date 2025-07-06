9f7d9ca8f224ae9b80a496d70aef1e845ed90410
nikop0 <nikosp005@gmail.com>
2025-07-06 14:48:28 +0200

    code updated

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let playBtn = document.querySelector('#play-btn');
 let chooseItem = document.querySelector('.choose-item');
 let moves = document.querySelectorAll('.single-item');
+let mainGame = document.querySelector('.main-game');
 let chosenMove = null;
 playBtn.addEventListener('click', e => {
     playBtn.style.display = 'none';
@@ -9,6 +10,15 @@ playBtn.addEventListener('click', e => {
 
 moves.forEach( move => {
     move.addEventListener('click', e => {
-    chosenMove = e.target;
+    chosenMove = e.target.closest('.single-item');
     chooseItem.style.display = 'none';
+    mainGame.style.display = 'flex';
+    console.log('');
+    mainGame.querySelector('.left').appendChild(chosenMove);
+    mainGame.querySelector('.right').appendChild(opponentMove());
 })});
+
+const opponentMove = () => {
+    let opMove = Math.floor(Math.random() * 3);
+    return moves[opMove];
+}
