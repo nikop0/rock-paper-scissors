d24d451809181d330cd1f773ffef4e900329e24b
nikop0 <nikosp005@gmail.com>
2025-07-09 22:49:01 +0200

    changes added

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ let moves = document.querySelectorAll('.single-item');
 let mainGame = document.querySelector('.main-game');
 let youWon = document.querySelector('#you-won');
 let youLost = document.querySelector('#you-lost');
+let myScore = document.querySelector('#my-score');
+let opScore = document.querySelector('#bot-score');
 let chosenMove = null;
+let myScoreNumber = parseInt(myScore.innerText);
+let opScoreNumber = parseInt(opScore.innerText);
 playBtn.addEventListener('click', e => {
     playBtn.style.display = 'none';
     chooseItem.style.display = 'block';
@@ -20,8 +24,12 @@ moves.forEach( move => {
     mainGame.querySelector('.right').appendChild(opMove);
     if(winner(chosenMove, opMove)) {
         youWon.style.display = 'block';
+        myScoreNumber++;
+        updateScore(myScoreNumber, opScoreNumber);
     } else {
         youLost.style.display = 'block';
+        opScoreNumber++;
+        updateScore(myScoreNumber, opScoreNumber);
     }
 })});
 
@@ -35,4 +43,9 @@ const winner = (p1, p2) => {
         return true;
     }
     return false;
+}
+
+const updateScore = (myS, opS) => {
+    myScore.innerText = myS;
+    opScore.innerText = opS;
 }
